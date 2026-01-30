3bcb3d687b387ccef7e5bd939d5e95b319641a4b
Ronny Wouters <ozzy.wouters@telenet.be>
2026-01-30 16:22:34 +0100

    Afspraken en berichten van Homepage naar myBnp (protected route) verplaatst

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,11 @@
-import {useLoginContext} from "../contexts/LoginContext";
-import {useMessagesContext} from "../contexts/MessagesContext";
 import {useNewsFlashesContext} from "../contexts/NewsFlashesContext";
 import {useLanguageContext} from "../contexts/LanguageContext";
-import {useAfsprakenContext} from "../contexts/AfsprakenContext";
 import {useCurrencyContext} from "../contexts/CurrencyContext";
 
 export default function Home() {
-    const {login} = useLoginContext();
-    const {messages} = useMessagesContext();
     const {newsFlashes} = useNewsFlashesContext();
     const {language} = useLanguageContext();
-    const {afspraken} = useAfsprakenContext()
-    const {rates, loading} = useCurrencyContext()
+    const {rates, loading} = useCurrencyContext();
 
     return (
         <>
@@ -32,31 +26,6 @@ export default function Home() {
                 ))}
             </ul>
 
-            <nav>
-                <>
-                    { login ?
-                        messages.length === 0 ? (
-                            <h2>{language === 'NL' ? 'Geen nieuws' : 'Pas des nouvelles'}</h2>
-                        ) : (
-                            <>  <h2>{language === 'NL' ? 'Berichten' : 'Messages'}</h2>
-                                {messages.map((m, index) => <p key={index}>{language === 'NL' ? m[0] : m[1]}</p>)}
-                            </>
-
-                        ) :null
-                    }
-
-                    { login ?
-                        afspraken.length === 0 ? (
-                            <h2>{language === 'NL' ? 'Geen afspraken' : 'Aucun rendez-vous'}</h2>
-                        ) : (
-                            <>  <h2>{language === 'NL' ? 'Afspraken' : 'Rendez-vous'}</h2>
-                                {afspraken.map((a, index) => <p key={index}>{a.datum} - {a.medewerker}</p>)}
-                            </>
-
-                        ) : null
-                    }
-                </>
-            </nav>
         </>
     );
 }
--- src/pages/MyBnp.js
+++ src/pages/MyBnp.js
@@ -1,9 +1,15 @@
 import {useLoginContext} from "../contexts/LoginContext";
-import { useNavigate } from "react-router-dom";
+import {useNavigate} from "react-router-dom";
+import {useMessagesContext} from "../contexts/MessagesContext";
+import {useAfsprakenContext} from "../contexts/AfsprakenContext";
+import {useLanguageContext} from "../contexts/LanguageContext";
 
-export function MyBnp(){
-    const { setLogin } = useLoginContext();
+export function MyBnp() {
+    const {login, setLogin} = useLoginContext();
     const navigate = useNavigate();
+    const {messages} = useMessagesContext();
+    const {afspraken} = useAfsprakenContext();
+    const {language} = useLanguageContext();
 
     const handleLogout = () => {
         setLogin(false)
@@ -14,6 +20,32 @@ export function MyBnp(){
         <div>
             <h2>MyBnp (Protected)</h2>
             <button onClick={handleLogout}>Logout</button>
+
+            <nav>
+                <>
+                    {login ?
+                        messages.length === 0 ? (
+                            <h2>{language === 'NL' ? 'Geen nieuws' : 'Pas des nouvelles'}</h2>
+                        ) : (
+                            <>  <h2>{language === 'NL' ? 'Berichten' : 'Messages'}</h2>
+                                {messages.map((m, index) => <p key={index}>{language === 'NL' ? m[0] : m[1]}</p>)}
+                            </>
+
+                        ) : null
+                    }
+
+                    {login ?
+                        afspraken.length === 0 ? (
+                            <h2>{language === 'NL' ? 'Geen afspraken' : 'Aucun rendez-vous'}</h2>
+                        ) : (
+                            <>  <h2>{language === 'NL' ? 'Afspraken' : 'Rendez-vous'}</h2>
+                                {afspraken.map((a, index) => <p key={index}>{a.datum} - {a.medewerker}</p>)}
+                            </>
+
+                        ) : null
+                    }
+                </>
+            </nav>
 
         </div>
     )
